047f9cacda1f63e8c283763dc595385d64da3d0a
Mauri2498 <pxnda2498@gmail.com>
2024-08-23 12:21:33 -0400

    modificando modelo usuario

--- application/models/usuario_model.php
+++ application/models/usuario_model.php
@@ -3,12 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Usuario_model extends CI_Model
 {
-	/* 	public function listausuarios()
-	{
-		$this->db->select('*');
-		$this->db->from('usuario');
-		return $this->db->get();  
-	} */
+
 	public function listausuarios()
 	{
 		$this->db->select('*');
