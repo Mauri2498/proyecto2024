7a59f938632fc7ef0b915334414ac482591d5275
Mauri2498 <pxnda2498@gmail.com>
2024-09-22 13:44:16 -0400

    aumentando el footer

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -4,9 +4,7 @@
 </aside>
 <!-- /.control-sidebar -->
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery -->
 <script src="<?php echo base_url(); ?>adminlte/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?php echo base_url(); ?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
